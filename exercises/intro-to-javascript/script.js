584b4c2cefed50101f82575932bc84ca33936483
Celeste Layne <layne.celeste@gmail.com>
2021-10-29 13:18:04 -0400

    Intro to Javascript

---
+++ exercises/intro-to-javascript/script.js
@@ -0,0 +1,17 @@
+/* 
+Part One
+1. Create three variables called noun verb and adjective and store one of each type.
+
+2. Choose a short one sentence poem that includes the following variables:
+—  Sample sentence: `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky:`
+
+3. Use the developer console to show the result.
+*/
+
+let noun = "cat";
+let verb = "purred";
+let adjective = "cuddly";
+
+let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`;
+
+console.log(sentence);
